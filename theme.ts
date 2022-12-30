4c62da6547f2ebd9c701cf6424334c13c237862e
Zero <zero@grepp.co>
2022-12-30 19:24:15 +0900

    feat: 시스템 다크모드 적용

---
+++ theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
